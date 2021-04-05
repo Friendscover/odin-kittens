fa460869d86f9717864062d9e6911d5c6cd9512e
Tobias Klöpper <tobiaskloepper1@web.de>
2021-04-05 20:21:52 +0200

    added CRUD views

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,6 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: true, length: { in: 4..32 }
+  validates :age, presence: true
+  validates :cuteness, presence: true, length: { in: 1..10 }
+  validates :softness, presence: true, length: { in: 1..10 }
 end
