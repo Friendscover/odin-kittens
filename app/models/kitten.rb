3e082df0741546ad6dbf8bbf9d32777f72e1bd96
Tobias Klöpper <tobiaskloepper1@web.de>
2021-04-05 21:19:45 +0200

    updated user input validation

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,6 +1,6 @@
 class Kitten < ApplicationRecord
   validates :name, presence: true, length: { in: 4..32 }
-  validates :age, presence: true
-  validates :cuteness, presence: true, length: { in: 1..10 }
-  validates :softness, presence: true, length: { in: 1..10 }
+  validates :age, presence: true, numericality: { only_integer: true, less_than: 200 }
+  validates :cuteness, presence: true, numericality: { only_integer: true, less_than: 10, greater_than: 0 }
+  validates :softness, presence: true, numericality: { only_integer: true, less_than: 10, greater_than: 0 }
 end
